eea2b38971fc3dd7560cbe723b376765dbe24ea5
redshark25 <tnsenurafernando@gmail.com>
2024-12-05 07:34:17 +0100

    buildingblocks

---
+++ buildingBlocks/questions/Mcq_buildingBlocks.java
@@ -0,0 +1,19 @@
+public class Mcq_buildingBlocks {
+    public static void main(String[] args) {
+        
+        var blocky = """
+                    squirrel \s
+                    pigeon \
+                    termite""";
+        System.out.print(blocky);
+       
+        System.out.println("\n");
+
+        var blocky2 = """
+            squirrel
+            pigeon 
+            termite""";
+            System.out.print(blocky2);
+    }
+
+}
