c0f6a4758bcb3493d4b9dd5f40838adaa1be9919
redshark25 <tnsenurafernando@gmail.com>
2025-02-10 17:17:05 +0100

    learning sorting in arrays

--- coreAPIs/src/array/Sorting.java
+++ coreAPIs/src/array/Sorting.java
@@ -1,4 +1,28 @@
 package array;
 
+import java.util.Arrays;
+
 public class Sorting {
+    public static void main(String[] args) {
+
+       int []numbers = {6, 9, 1};
+       Arrays.sort(numbers);
+
+        for(int i = 0; i < numbers.length; i++)
+            System.out.println(numbers[i]);
+
+        System.out.println(numbers);
+        System.out.println(java.util.Arrays.toString(numbers));
+        System.out.println(Arrays.toString(numbers)); // print the array using Arrays
+
+
+        String[] stringArray = {"10", "9", "100"};
+        Arrays.sort(stringArray);
+        System.out.println(Arrays.toString(stringArray));// String sort thing in alphabetic order
+        // numbers before letters.  uppercase before lowercase
+
+      //  int[] randomNumbers = {2, 3, 4, 5, 6, null}; <- null can not be placed in int
+        String randomString[] = {null};
+
+    }
 }
