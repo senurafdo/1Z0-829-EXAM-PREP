73c1858830212e3cc91cd32fed36fcf36f96ed02
redshark25 <tnsenurafernando@gmail.com>
2025-02-22 00:04:20 +0100

    learn collections

--- collectionAndGenerics/src/Deque/Test.java
+++ collectionAndGenerics/src/Deque/Test.java
@@ -1,4 +1,17 @@
 package Deque;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Test {
+    public static void main(String[] args) {
+
+        var testDeque = new ArrayDeque<Integer>();
+        testDeque.offer(18);
+        testDeque.offer(5);
+
+        System.out.println(testDeque);
+
+
+    }
 }
--- collectionAndGenerics/src/generics/genericInterfaces/Main.java
+++ collectionAndGenerics/src/generics/genericInterfaces/Main.java
@@ -1,4 +1,9 @@
 package generics.genericInterfaces;
 
+import java.util.ArrayList;
+
 public class Main {
+    public static void main(String[] args) {
+        //ArrayList<Number> list = new ArrayList<Integer>();
+    }
 }
